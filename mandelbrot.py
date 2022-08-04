94c0727cf5c9e0df7fdf4ab616f0bff31136e858
andrewcara <64450522+andrewcara@users.noreply.github.com>
2022-08-04 15:51:27 -0400

    initial

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,6 +3,7 @@ from matplotlib.pyplot import plot
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import matplotlib.cm as cm
 
 
 def complex_matrix(xmin, xmax, ymin, ymax, pixel_density):
@@ -12,17 +13,36 @@ def complex_matrix(xmin, xmax, ymin, ymax, pixel_density):
 
 def is_stable(c, num_iterations):
     z = 0 #number of iterations is 20 so there is sufficient testing to see of the result is unbounded
-    for _ in range(num_iterations): #vectorization function will iterate through every value in c
+    for _ in range(num_iterations): #vectorization function will iterate through c to determine if the value is not bounded
         z = z ** 2 + c #mandelbrot equation
     return abs(z) <= 2 # generator that will return either true or false for the point that it is located
 
+def julia(c1):
+   c = -0.1 +0.65j
+   to_plot = np.zeros((300,400))
+   for i in range(len(c1)):
+    for j in range(len(c1[0])):
 
-c = complex_matrix(-2, 0.5, -1.5, 1.5, pixel_density=600)
+        nit = 0
+        z = c1[i][j]
 
-print(c)
+        while abs(z) < 10 and nit < 1000:
+            z = z **2 + c
+            nit += 1
+        shade = 1-np.sqrt(nit / 1000)
+        ratio = shade / 1000
+        to_plot[i][j] = ratio
+   return to_plot
 
-plt.imshow(is_stable(c, num_iterations=15), cmap="binary")
-plt.gca().set_aspect("equal")
-plt.axis("off")
-plt.tight_layout()
+c = complex_matrix(-2, 2, -1.5, 1.5, pixel_density=100)
+
+
+x = julia(c)
+print(x)
+plt.imshow(x,interpolation='nearest', cmap=cm.hot)
 plt.show()
+# plt.imshow(is_stable(c, num_iterations=15), cmap="binary")
+# #plt.imshow(julia_set(c, num_iterations=15), cmap="binary")
+# plt.gca().set_aspect("equal")
+# plt.axis("off")
+# plt.tight_layout()
